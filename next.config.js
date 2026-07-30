d8d97dfa3e90df723c36eab36fd3587add8c178c
kakani17481a0566 <84347625+kakani17481a0566@users.noreply.github.com>
2026-07-30 18:58:23 +0530

    ws

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/igs-school",
+        destination: "/igs-school/index.html",
+      },
+      {
+        source: "/igs-school/",
+        destination: "/igs-school/index.html",
+      },
+    ];
+  },
+};
 
 export default nextConfig;
